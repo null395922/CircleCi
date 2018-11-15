3cb47f3b3a4626164a53cee3a395f6ae460a56df
Nate Johnson <nathan@meshify.com>
2018-11-15 11:17:13 -0600

    add multiplication

--- sketch.test.js
+++ sketch.test.js
@@ -1,5 +1,8 @@
-const sum = require('./sketch');
+const { sum, prod } = require('./sketch');
 
 test('adds 1 + 2 to equal 3', function() {
   expect(sum(1, 2)).toBe(3);
+});
+test('multiplies 4 and 2 to equal 8', function() {
+  expect(sum(4, 2)).toBe(8);
 });
