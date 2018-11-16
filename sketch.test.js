a2ca434164b57b1a729215f3e2e7dcdfde090ca7
Nate Johnson <nathan@meshify.com>
2018-11-16 11:26:11 -0600

    false mult test

--- sketch.test.js
+++ sketch.test.js
@@ -4,5 +4,5 @@ test('adds 1 + 2 to equal 3', function() {
   expect(sum(1, 2)).toBe(3);
 });
 test('multiplies 4 and 2 to equal 8', function() {
-  expect(prod(4, 2)).toBe(8);
+  expect(prod(4, 2)).toBe(3);
 });
